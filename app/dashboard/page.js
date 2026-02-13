5027394133d58ad65422258231144f441c6a9e2c
R Pradhyumna <pradhyumnar26@gmail.com>
2026-02-13 22:26:27 +0530

    Added rangetraccker to filter based on time

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,38 +1,45 @@
+import RangeSelector from "@/app/_components/RangeSelector";
 import { Suspense } from "react";
 import ChartsWrapper from "./_sections/ChartsWrapper";
 import KpiSectionWrapper from "./_sections/KpiSectionWrapper";
 import TableWrapper from "./_sections/TableWrapper";
-
 export default function DashboardPage({ searchParams }) {
   return (
-    <div className="flex flex-col gap-8">
-      <Suspense
-        fallback={
-          <div className="h-32 bg-slate-200 dark:bg-slate-700 rounded animate-pulse" />
-        }
-      >
-        <section id="overview">
-          <KpiSectionWrapper searchParams={searchParams} />
-        </section>
-      </Suspense>
+    <>
+      <div className="flex justify-between items-center">
+        <h2>Overview</h2>
+        <RangeSelector />
+      </div>
 
-      <Suspense
-        fallback={
-          <div className="h-100 bg-slate-200 dark:bg-slate-700 rounded animate-pulse" />
-        }
-      >
-        <ChartsWrapper searchParams={searchParams} />
-      </Suspense>
+      <div className="flex flex-col gap-8">
+        <Suspense
+          fallback={
+            <div className="h-32 bg-slate-200 dark:bg-slate-700 rounded animate-pulse" />
+          }
+        >
+          <section id="overview">
+            <KpiSectionWrapper searchParams={searchParams} />
+          </section>
+        </Suspense>
 
-      <Suspense
-        fallback={
-          <div className="h-125 bg-slate-200 dark:bg-slate-700 rounded animate-pulse" />
-        }
-      >
-        <section id="table">
-          <TableWrapper searchParams={searchParams} />
-        </section>
-      </Suspense>
-    </div>
+        <Suspense
+          fallback={
+            <div className="h-100 bg-slate-200 dark:bg-slate-700 rounded animate-pulse" />
+          }
+        >
+          <ChartsWrapper searchParams={searchParams} />
+        </Suspense>
+
+        <Suspense
+          fallback={
+            <div className="h-125 bg-slate-200 dark:bg-slate-700 rounded animate-pulse" />
+          }
+        >
+          <section id="table">
+            <TableWrapper searchParams={searchParams} />
+          </section>
+        </Suspense>
+      </div>
+    </>
   );
 }
